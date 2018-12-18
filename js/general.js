278e3f0b41dbd43e27bc94a70ed33c50ddc0b497
deight48 <42502466+deight48@users.noreply.github.com>
2018-12-18 05:47:15 -0500

    Update general.js

--- js/general.js
+++ js/general.js
@@ -1,8 +1,8 @@
 window.onload = function () {
-    var header = ["Nombre", "Apellido", "Direccion", "Telefono"];
-    var valores = [["Deight", "Paredes", "Cota 905 El Naranjal", "960116855"],
-        ["Darrin", "Paredes", "Cota 905 El Naranjal", "980555876"],
-        ["Darrin", "Paredes", "Cota 905 El Naranjal", "980555876"],
-        ["Darrin", "Paredes", "Cota 905 El Naranjal", "980555876"]];
-    var jtable = new CreateJTable("JTable", header, valores, "green");
+    var header = ["header", "header", "header", "header"];
+    var valores = [["texto", "texto", "texto", "texto"],
+        ["texto", "texto", "texto", "texto"],
+        ["texto", "texto", "texto", "texto"],
+        ["texto", "texto", "texto", "texto"]];
+    var jtable = new CreateJTable("JTable", header, valores, "blue");
 };
